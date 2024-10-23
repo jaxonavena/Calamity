fa0a700cfa6853d3d9aafb08f98eb2a7bda5dfb3
Jaxon Avena <jaxonavena@gmail.com>
2024-10-23 17:27:17 -0500

    Add character class

---
+++ calamity-godotproject/characters/character.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+class_name Character
+
+const FRICTION: float = 0.15
+@export var acceleration: int = 40
+@export var max_speed: int = 100
+
+var mov_direction: Vector2 = Vector2.ZERO
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(_delta: float) -> void:
+	velocity = velocity.move_toward(Vector2.ZERO, FRICTION * max_speed * _delta)
+	move_and_slide()
+
+func move() -> void:
+	mov_direction = mov_direction.normalized()
+	velocity += mov_direction * acceleration
+	velocity = velocity.limit_length(max_speed)
