2523e85a3cae3917895db6c22bfafee08f0f5cc6
Jaxon Avena <jaxonavena@gmail.com>
2024-11-12 23:07:52 -0600

    Got random generation working. The dungeon's dont lien up exactly right. Got some random enemy generation going, but not on a timer. There's a weird bug where certain tiles have a hitbox, but I don't think they ever actually got a hitbox added.

--- calamity-godotproject/scenes/dungeon_generator.gd
+++ calamity-godotproject/scenes/dungeon_generator.gd
@@ -1,27 +1,53 @@
 extends Node2D
 
 @export var room_scenes: Array = []
+var player = preload("res://scenes/player.tscn")
+var enemy = preload("res://scenes/enemy.tscn")
 var used_positions = {}
 
 func _ready():
 	generate_dungeon()
+	place_player()
+	place_enemies(10)
+	
+func _process(delta):
+	# Check for Cmd+R or Ctrl+R press to restart the scene
+	if Input.is_action_just_pressed("restart_scene"):
+		restart_scene()
+	
+func restart_scene():
+	# Reload the current scene
+	var current_scene = get_tree().current_scene
+	get_tree().reload_current_scene()
 	
 func generate_dungeon():
 	# Initialize the starting room
 	var start_room = instance_room()
 	start_room.position = Vector2.ZERO  # Starting at (0, 0)
 	add_child(start_room)
+	
 	used_positions[start_room.position] = start_room
+	print("start room positiion: ")
+	print(start_room.position)
 
 	# Start placing connected rooms
 	place_connected_rooms(start_room, 5)  # Adjust number of rooms
 
 
-#func drop_items():
-	#var item_instance = coin_bag.instantiate()
-	#item_instance.global_position = $Marker2D.global_position
-	#get_parent().add_child(item_instance)
+func place_player():
+	# Create and position the player
+	var player_instance = player.instantiate()
+	player_instance.position = Vector2(50, 50)  #set position
+	add_child(player_instance)
 	
+	
+func place_enemies(count: int):
+	for i in range(count):
+		# Create and position the enemy
+		var enemy_instance = enemy.instantiate()
+		enemy_instance.position = Vector2(randi_range(0, 1500), randi_range(0, 1500))  #set position
+		add_child(enemy_instance)
+
 	
 func instance_room() -> Node2D:
 	# Choose a random room scene to instance
@@ -52,6 +78,10 @@ func place_connected_rooms(current_room: Node2D, remaining_rooms: int):
 func get_door_markers(room: Node2D) -> Array:
 	var door_markers = []
 	for child in room.get_children():
-		if child is Marker2D and child.name.begins_with("Door"):
+		if child is Marker2D and child.name.begins_with("D"):
 			door_markers.append(child)
+	print("Getting this room's doors:")
+	print(room)
+	print("door markers:")
+	print(door_markers)
 	return door_markers
