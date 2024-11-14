1412cd664ed59e368084c5d16ef5579fc6a71575
Jaxon Avena <jaxonavena@gmail.com>
2024-11-14 01:00:15 -0600

    Add stat display. Broken style

--- calamity-godotproject/scripts/global.gd
+++ calamity-godotproject/scripts/global.gd
@@ -1,15 +1,26 @@
 extends Node
-
+# PLAYER FLAGS
 var player_current_attack = false
+var player_can_use_stairs = false
+
+# PLAYER STATS
 var player_coins = 0
 var player_xp = 0
-var player_can_use_stairs = false
-var player_instance = null
-var previous_floor = null
 var floors_cleared = 0
 var time_survived = 0.0
 var kills = 0
 
+func reset_player_stats():
+	player_xp = 0
+	floors_cleared = 0
+	time_survived = 0
+	kills = 0
+
+# PLAYER PERSISTENCE
+var player_instance = null
+var previous_floor = null
+
 func new_floor():
+	floors_cleared += 1
 	previous_floor.remove_child(player_instance)
 	get_tree().change_scene_to_file("res://scenes/dungeon_generator.tscn")
